6650d09e0d117b4c21090db61b68571ce7874550
satyajeet104 <satyajeet104@github.com>
2024-12-02 13:09:51 +0530

    Init repo with solutions to 1 and 2

---
+++ src/1.rs
@@ -0,0 +1,41 @@
+use itertools::{Either, Itertools};
+use std::{collections::HashMap, io::Read};
+
+fn main() -> anyhow::Result<()> {
+    let filepath = "tests/1/1.txt";
+    let mut contents = String::new();
+    let _ = std::fs::File::open(filepath)?.read_to_string(&mut contents)?;
+
+    let (mut l1, mut l2): (Vec<_>, Vec<_>) = contents
+        .split_ascii_whitespace()
+        .map(|c| c.parse::<i64>().unwrap())
+        .enumerate()
+        .partition_map(|(i, v)| {
+            if i % 2 == 0 {
+                Either::Left(v)
+            } else {
+                Either::Right(v)
+            }
+        });
+
+    l1.sort();
+    l2.sort();
+
+    let ans = std::iter::zip(l1.clone(), l2.clone())
+        .fold(0, |acc, (x, y)| acc + (x - y).abs());
+
+    println!("Part 1 ans: {ans}");
+
+    let freq_map = l2.into_iter().fold(HashMap::new(), |mut map, val| {
+        *map.entry(val).or_insert(0) += 1;
+        map
+    });
+
+    let ans = l1
+        .into_iter()
+        .fold(0, |acc, e| (e * freq_map.get(&e).unwrap_or(&0)) + acc);
+
+    println!("Part 2 ans: {ans}");
+
+    Ok(())
+}
---
+++ src/2.rs
@@ -0,0 +1,65 @@
+use std::io::Read;
+
+fn is_level_okay(cur: &[i64], skip: Option<usize>) -> bool {
+    let mut is_okay = true;
+    let (mut pos, mut neg) = (false, false);
+    let mut prev = None;
+    for i in 0..cur.len() {
+        if matches!(skip, Some(ii) if ii == i) {
+            continue;
+        }
+        if prev.is_none() {
+            prev = Some(i);
+            continue;
+        }
+        let diff = cur[i] - cur[prev.unwrap()];
+        if diff > 0 {
+            pos = true;
+        } else {
+            neg = true;
+        }
+        let e = diff.abs();
+        if !(1..=3).contains(&e) {
+            is_okay = false;
+            break;
+        }
+        prev = Some(i);
+    }
+    is_okay &= pos ^ neg;
+    is_okay
+}
+
+fn main() -> anyhow::Result<()> {
+    let filepath = "tests/2/1.txt";
+    let mut contents = String::new();
+    let _ = std::fs::File::open(filepath)?.read_to_string(&mut contents)?;
+    let levels = contents
+        .split("\n")
+        .map(|s| {
+            s.split_ascii_whitespace()
+                .map(|ns| ns.parse::<i64>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let ans = levels.iter().fold(0, |acc, cur| {
+        is_level_okay(cur, None) as usize + acc
+    });
+
+    println!("Part 1 ans: {ans}");
+
+    let ans = levels.iter().fold(0, |acc, cur| {
+        let mut is_okay = false;
+        for skip in 0..cur.len() {
+            if is_level_okay(cur, Some(skip)) {
+                is_okay = true;
+                break;
+            }
+        }
+        acc + (is_okay as usize)
+    });
+
+    println!("Part 2 ans: {ans}");
+
+    Ok(())
+}
