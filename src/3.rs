81f88c0efa12cb7a57dda06294fccdb6747c01b6
blunderbussatya <blunderbussatya@github.com>
2024-12-03 12:54:42 +0530

    Simplify 3rd soln

--- src/3.rs
+++ src/3.rs
@@ -6,8 +6,8 @@ fn get_muls(input: &str) -> anyhow::Result<i64> {
     let regex = Regex::new(mul_pattern)?;
     let ans = regex
         .captures_iter(input)
-        .map(|c| (c[1].parse::<i64>().unwrap(), c[2].parse::<i64>().unwrap()))
-        .fold(0, |acc, (n1, n2)| acc + n1 * n2);
+        .map(|c| c[1].parse::<i64>().unwrap() * c[2].parse::<i64>().unwrap())
+        .sum();
     Ok(ans)
 }
 
@@ -17,9 +17,10 @@ fn main() -> anyhow::Result<()> {
     let _ = std::fs::File::open(filepath)?.read_to_string(&mut contents)?;
     let ans = get_muls(&contents)?;
     println!("Part 1 ans: {ans}");
-    let ans = contents.split("do()").fold(0, |acc, dos| {
-        acc + get_muls(dos.split("don't()").next().unwrap()).unwrap()
-    });
+    let ans: i64 = contents
+        .split("do()")
+        .map(|dos| get_muls(dos.split("don't()").next().unwrap()).unwrap())
+        .sum();
     println!("Part 2 ans: {ans}");
     Ok(())
 }
